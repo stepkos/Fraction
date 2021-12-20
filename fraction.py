5c003f50cbe9141d0544c55210bf353e263f8aa8
stepkos <stepkos@wp.pl>
2021-12-20 01:09:51 +0100

    Decorator signature as static method

--- fraction.py
+++ fraction.py
@@ -1,15 +1,3 @@
-def argumentToFraction(func):
-    def wrapper(self, other):
-        if type(other) == type(self):
-            pass
-        elif type(other) == int:
-            other = Fraction.fromInt(other)
-        else:
-            raise TypeError
-        return func(self, other)
-    return wrapper
-
-
 class Fraction:
     gcd = staticmethod(lambda a, b: Fraction.gcd(b, a % b) if b else a)
     lcm = staticmethod(lambda a, b: a * b / Fraction.gcd(a, b))
@@ -56,6 +44,19 @@ class Fraction:
     def getValues(self):
         return self.num, self.den
 
+    @staticmethod
+    def __argumentToFraction(func):
+        def wrapper(self, other):
+            if type(other) == type(self):
+                pass
+            elif type(other) == int:
+                other = Fraction.fromInt(other)
+            else:
+                raise TypeError
+            return func(self, other)
+
+        return wrapper
+
     # x + y -> x.__add__(y)
     def __add__(self, other):
         pass
@@ -65,7 +66,7 @@ class Fraction:
         pass
 
     # x * y -> x.__mul__(y)
-    @argumentToFraction
+    @__argumentToFraction.__get__('')
     def __mul__(self, other):
         num, den = self.getValues()
         num *= other.num
@@ -73,7 +74,7 @@ class Fraction:
         return Fraction.hotReturn(num, den)
 
     # x / y -> x.__truediv__(y)
-    @argumentToFraction
+    @__argumentToFraction.__get__('')
     def __truediv__(self, other):
         num, den = self.getValues()
         den *= other.num
